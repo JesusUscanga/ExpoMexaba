e6a262e05bffe6f88d9b988700363a731d8f0efb
JesusUscanga <jesususcanga24@gmail.com>
2021-07-09 22:42:22 -0500

    Login Contraseña General

---
+++ Login/Login2.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <title>Login</title>
+
+    <style>
+        body {
+            background: #ffe259;
+            background: linear-gradient(to right, #ffa751, #ffe259);
+        }
+        
+        .bg {
+            background-image: url(img/stand.png);
+            background-position: center center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4"></div>
+        </div>
+    </div>
+</body>
+<script src="bootstrap/js/popper.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<!-- Components 
+<script src="../bootstrap/js/popper.min.js"></script>
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+<script src="../bootstrap/js/bootstrap.bundle.min.js"></script>-->
+
+</html>
--- index.php
+++ index.php
@@ -1,35 +1,3 @@
 <?php
-
-    session_start();
-
-    require_once 'ClasesPHP/Clases.php';
-    $Clases = new Clases();
-
-    $revisa = $Clases->RevisarCambios();
-
-    if(isset($_SESSION) && isset($_SESSION['logueado']) == TRUE){
-        header('Location: secciones/');
-    }
-
-    #Debemos de verificar el envio del formulario
-    if(isset($_POST['btnLogin'])){
-        $idusuario = $_POST['idusuario'];
-        $idpassword = $_POST['password'];
-
-        $resultSet = $Clases->select($idusuario, $idpassword);
-
-        if($resultSet->num_rows > 0){
-            $row = $resultSet->fetch_assoc();
-            $_SESSION['logueado'] = TRUE;
-            $_SESSION['userName'] = $row['user'];
-            //$_SESSION['userName'] ="Nombre de usuario";
-        
-            //echo "Bienvenido ".$userName." al sistema de alumnos ";
-            header("Location: secciones/index.php");
-        }else{
-            $errmensaje = "Usuario o contraseña invalidos";
-            //echo $errmensaje;
-        }
-    }
     require_once 'login/Login.php';
     ?>

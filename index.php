b5409ab5a7f3e014def927635b6353068239b71e
JesusUscanga <jesususcanga24@gmail.com>
2021-07-19 21:17:25 -0500

    Secciones interactivas

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
  session_start();
 
  if(isset($_SESSION) && isset($_SESSION['logueado']) == TRUE){
-    header('Location: secciones/');
+    header('Location: Secciones/');
 }else{
-    require_once 'login/Login.php';
+    require_once 'Login/Login.php';
 }
 
 if(isset($_POST['passwordhidden'])){
     $pass = $_POST['passwordhidden'];
     if($pass == "expo2021"){
         $_SESSION['logueado'] = TRUE;
-        header('Location: secciones/');
+        header('Location: Secciones/');
     }else{
         $_SESSION['logueado'] == FALSE;
     }
